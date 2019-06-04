45ffd9fbdce08ded648032eac0d1d74fb457e895
David Alexander <alexanda@galileo.txcorp.com>
2019-06-04 15:01:03 -0600

    Revert "Merge branch '2.13RC' of https://github.com/Tech-XCorp/visit into 2.13RC"
    This reverts commit 3c6ee18064671009538b3c7336db4af79b57a0b6, reversing
    changes made to de1fefc153630f0071664ed87fca22c4a47f73f3.

--- src/databases/Vs/VsGroup.h
+++ src/databases/Vs/VsGroup.h
@@ -1,11 +1,14 @@
 /**
  * @file VsGroup.h
+ *      
+ *  @class VsGroup
+ *  @brief Represents an hdf5 group from an hdf5 file.
+ *  
+ *  Provides accessors to get attributes and datasets 
+ *  from a group in an hdf5 file.
  *
- * @brief Represents an hdf5 group from an hdf5 file.
- *
- * Provides accessors to get attributes and datasets
- * from a group in an hdf5 file.
- *
+ *  Created on: Apr 27, 2010
+ *      Author: mdurant
  */
 
 #ifndef VS_GROUP_H_
@@ -22,17 +25,12 @@ class VsAttribute;
 class VsDataset;
 
 class VsGroup : public VsObject {
-
-  public:
-
-    VsGroup(VsRegistry* r, VsObject* parentObject, std::string groupName,
-        hid_t id);
-
-    virtual ~VsGroup();
-
-    void write() const;
-
+public:
+  VsGroup(VsRegistry* r, VsObject* parentObject, std::string groupName, hid_t id);
+  virtual ~VsGroup();
+  
+  void write() const;
+  
 };
 
-#endif // VS_GROUP_H_
-
+#endif /* VS_GROUP_H_ */
