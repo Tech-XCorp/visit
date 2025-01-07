07df1a08a122e8874c2ff8a1537e88b12641c90c
John Cary <cary@txcorp.com>
2025-01-07 16:40:41 -0700

    Change int to ssize_t for number of mesh points. This fixes a memory corruption bug caused by loading a structured mesh with enough cells to overflow a 32-bit integer value. Switching to ssize_t to ensure that we have enough room for larger values, while still allowing the variable to hold a negative value for error signalling and loops that use i--.

--- src/databases/Vs/avtVsFileFormat.C
+++ src/databases/Vs/avtVsFileFormat.C
@@ -1117,8 +1117,8 @@ vtkDataSet* avtVsFileFormat::getStructuredMesh(VsReader* reader,
     haveDataSelections = 1;
 #endif
 
-    int numPoints = 1;
-    for (size_t i=0; i<numSpatialDims; ++i)
+    ssize_t numPoints = 1;
+    for (ssize_t i=0; i<numSpatialDims; ++i)
       numPoints *= gdims[i];
 
     VsLog::debugLog() << CLASSFUNCLINE
@@ -1183,7 +1183,7 @@ vtkDataSet* avtVsFileFormat::getStructuredMesh(VsReader* reader,
                           << numSpatialDims << " is less than 3.  "
                           << "Moving data into correct location." << std::endl;
 
-        for (int i=numPoints-1; i>=0; --i)
+        for (ssize_t i=numPoints-1; i>=0; --i)
         {
             unsigned char* destPtr
               = (unsigned char*) dataPtr + i*3*dsize;
@@ -1235,22 +1235,6 @@ vtkDataSet* avtVsFileFormat::getStructuredMesh(VsReader* reader,
         delete [] fltDataPtr;
     }
 
-    /*
-     // debug: output points
-     fltDataPtr = (float*)ptsPtr;
-     dblDataPtr = (double*)ptsPtr;
-     for (size_t i = 0; i < numPoints; ++i) {
-     VsLog::debugLog() << i << ":";
-     for (size_t j = 0; j < 3; ++j) {
-     if (H5Tequal(type, H5T_NATIVE_DOUBLE)) {
-     VsLog::debugLog() << " " << dblDataPtr[(3*i)+j];
-     }
-     else VsLog::debugLog() << " " << fltDataPtr[(3*i)+j];
-     }
-     VsLog::debugLog() << std::endl;
-     }
-     // end debug
-     */
     vtkStructuredGrid* sgrid = vtkStructuredGrid::New();
     sgrid->SetDimensions(&(gdims[0]));
     sgrid->SetPoints(vpoints);
